cb25fa2aad4ce316d0bbd152e292c72ad68a3b61
Roman Kashitsyn <roman@dfinity.org>
2019-11-06 14:09:02 +0100

    Change copyright holder to DFINITY.

--- src/id.rs
+++ src/id.rs
@@ -1,4 +1,4 @@
-// Copyright 2019 (c) Roman Kashitsyn
+// Copyright 2019 DFINITY
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
